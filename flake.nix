f8cf8719602b9b3ff3dbaece8eac3440638b8270
hollowillow <188023203+hollowillow@users.noreply.github.com>
2026-04-13 14:34:31 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+    description = "Nix flake shell scripting environment";
+    inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+    outputs = { self , nixpkgs ,... }: let
+        system = "x86_64-linux";
+    in {
+        devShells."${system}".default = let
+            pkgs = import nixpkgs { inherit system; };
+        in pkgs.mkShell {
+            packages = with pkgs; [
+                shellcheck
+                bash-language-server
+                dash
+            ];
+            shellHook = ''
+                exec fish
+            '';
+        };
+    };
+}
